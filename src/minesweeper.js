075270db29f99cd671ad94731d431bbf993aff18
Arnold Redoblado <VOODOO15@VOODOO15-MBP.lan>
2017-09-24 17:31:57 -0400

    Replaced board mockup with printBoard function with mockup for user entry.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,20 @@
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
 
-console.log(`This is what an empty board would look like:`);
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+const printBoard = (board) => {
+  console.log(`Current Board:`);
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+}
 
-console.log(`This is what an empty board would look like:`);
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);

14a3c23a9f8ea67a010ebda07d16cc9ab385eb19
Arnold Redoblado <VOODOO15@VOODOO15-MBP.lan>
2017-09-29 02:48:12 -0400

    Removed original code in minesweeper.js.  Created generatePlayerBoard, generateBombBoard and refactored printBoard functions.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,20 +1,55 @@
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 
-const printBoard = (board) => {
-  console.log(`Current Board:`);
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+  let board = [];
+
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+
+    board.push(row);
+
+  }
+
+  return board;
+
 }
 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 
-printBoard(board);
+  let board = [];
 
-board[0][1] = '1';
-board[2][2] = 'B';
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
 
-printBoard(board);
+
+  let numberOfBombsPlaced = 0, randomRowIndex = 0, randomColumnIndex = 0;
+
+  while (numberOfBombsPlaced < numberOfBombs) {
+    randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    //While loop has potential to place bombs on top of already existing bombs
+  }
+
+  return board;
+}
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+const playerBoard = generatePlayerBoard(3, 4);
+const bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+printBoard(bombBoard);
